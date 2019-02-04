55d8d6cbe4a09e57aa0d7c1d0551fefb1e351f50
manon jacquin <manon.jacquin@gmail.com>
2019-02-04 11:06:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === "I love u, Grandma.") return `I love you, too.`
   else if (string === string.toLowerCase()) return `I can't hear you!`
-  else return `YES INDEED
+  else return `YES INDEED!`
 }
